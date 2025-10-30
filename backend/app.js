ef23923be2c778d13f4a1639a14a5ebb78af8f8e
Ramiro <ramiroroman306@gmail.com>
2025-10-30 15:05:02 -0300

    Conexion a la base de datos

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+// Importaciones
+import express from "express";
+import "dotenv/config"
+import { connectDB } from "./config/database.js";
+import cookieParser from "cookie-parser";
+
+// Configuraciones
+const app = express();
+const PORT = process.env.PORT;
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+//Rutas
+// app.use("/api", routes);
+
+//Conexion a la base de datos
+app.listen(PORT, async () => {
+    console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
+    await connectDB();
+});
