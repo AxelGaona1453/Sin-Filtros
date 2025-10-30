2ead91fcc88adc0b18a4456aaf0bea1854f6e288
Ramiro <ramiroroman306@gmail.com>
2025-10-30 16:15:47 -0300

    Se creo el registro de usuario, junto con su ruta, se creo el validator.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import "dotenv/config"
 import { connectDB } from "./config/database.js";
 import cookieParser from "cookie-parser";
+import { routes } from "./routes/index.js";
 
 // Configuraciones
 const app = express();
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 //Rutas
-// app.use("/api", routes);
+app.use("/sin-filtros", routes);
 
 //Conexion a la base de datos
 app.listen(PORT, async () => {
---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,31 @@
+import { comparePassword, hashPassword } from "../helpers/bcrypt.helper.js";
+import { generateToken } from "../helpers/jwt.helper.js";
+import { UserModel } from "../models/user.model.js";
+import {matchedData} from "express-validator"
+
+export const register = async (req,res) => {
+    try {
+        const data = matchedData(req);
+
+        const hashedPassword = await hashPassword(data.password);
+
+        const user = await UserModel.create({
+            username: data.username,
+            email: data.email,
+            password: hashedPassword,
+            profile: data.profile,
+        })
+
+        res.status(201).json({
+            ok: true,
+            msg: "Usuario creado con exito",
+            user
+        })
+    } catch (err) {
+        res.status(500).json({
+            ok: false,
+            msg: "Error interno del servidor",
+            err,
+        })
+    }
+}
