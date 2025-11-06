09bd5d0acf6efbce76af0212c401458fe4b47ed3
axelon <207878001+NatsuX1453@users.noreply.github.com>
2025-11-06 14:45:15 -0300

    se crearon los demas componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './Dashboard';
+import Dashboard from '../pages/LoginPage';
 
 function App() {
 	return (
