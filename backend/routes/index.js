d8a3de2663c1a87c92146c3f8bfac81787924048
Ramiro <ramiroroman306@gmail.com>
2025-11-15 12:26:41 -0300

    Posibles notificaciones push

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,16 @@ import { Router } from "express";
 import { authRoutes } from "./auth.routes.js";
 import { postRoutes } from "./post.routes.js";
 import { aiRoutes } from "./ai.routes.js";
+import { notificationsRoutes } from "./notifications.routes.js";
 
 // Router principal
 export const routes = Router();
 
-// Rutas auth
+// Rutas Auth
 routes.use(authRoutes);
+// Rutas Post
 routes.use(postRoutes);
+// Rutas IA
 routes.use(aiRoutes);
+// Rutas Notificaciones
+routes.use(notificationsRoutes)
